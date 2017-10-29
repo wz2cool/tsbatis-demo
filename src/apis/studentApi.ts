0005706f980f2ddf340f37113de1f24e2d4de424
wz2cool <wz2cool@live.cn>
2017-10-29 23:22:53 +0800

    add

--- src/apis/studentApi.ts
+++ src/apis/studentApi.ts
@@ -31,31 +31,28 @@ export class StudentApi {
                     res.status(500).send(err);
                 });
         });
-        return studentApi;
-    }
 
-    private async getStudents(): Promise<Student[]> {
-        let connection: IConnection;
-        try {
-            connection = await this.connectionFactory.getConnection();
-            const studentMapper = new StudentMapper(connection);
-            const query = DynamicQuery.createIntance<Student>();
-            const idDescSort = new SortDescriptor<Student>((s) => s.id, SortDirection.DESC);
-            query.addSorts(idDescSort);
-            const students = await studentMapper.selectByDynamicQuery(query);
-            return new Promise<Student[]>((resolve, reject) => resolve(students));
-        } catch (e) {
-            return new Promise<Student[]>((resolve, reject) => reject(e));
-        } finally {
-            if (connection) {
-                try {
-                    await connection.release();
-                } catch (releaseError) {
-                    // nothing to do.
-                    console.error(releaseError);
-                }
-            }
-        }
+        studentApi.delete("/:id", (req, res, next) => {
+            const id = req.params.id;
+            this.deleteStudent(id)
+                .then(() => {
+                    res.json("success");
+                }).catch((err) => {
+                    res.status(500).send(err);
+                });
+        });
+
+        studentApi.put("/", (req, res, next) => {
+            const id = req.params.id;
+            const newStudents = MappingProvider.toEntities<Student>(Student, req.body, false);
+            this.updateStudent(newStudents)
+                .then(() => {
+                    res.json("success");
+                }).catch((err) => {
+                    res.status(500).send(err);
+                });
+        });
+        return studentApi;
     }
 
     private async addStudents(students: Student[]): Promise<number[]> {
@@ -84,6 +81,87 @@ export class StudentApi {
             }
         } catch (e) {
             return new Promise<number[]>((resolve, reject) => reject(e));
+        } finally {
+            if (connection) {
+                try {
+                    await connection.release();
+                } catch (releaseError) {
+                    // nothing to do.
+                    console.error(releaseError);
+                }
+            }
+        }
+    }
+
+    private async deleteStudent(id: number): Promise<void> {
+        let connection: IConnection;
+        try {
+            connection = await this.connectionFactory.getConnection();
+            const studentMapper = new StudentMapper(connection);
+            await studentMapper.deleteByPrimaryKey(id);
+            return new Promise<void>((resolve, reject) => resolve());
+        } catch (e) {
+            return new Promise<void>((resolve, reject) => reject(e));
+        } finally {
+            if (connection) {
+                try {
+                    await connection.release();
+                } catch (releaseError) {
+                    // nothing to do.
+                    console.error(releaseError);
+                }
+            }
+        }
+    }
+
+    private async updateStudent(students: Student[]): Promise<void> {
+        if (!students || students.length === 0) {
+            return new Promise<void>((resolve, reject) => resolve());
+        }
+
+        let connection: IConnection;
+        try {
+            connection = await this.connectionFactory.getConnection();
+            await connection.beginTransaction();
+            try {
+                const studentMapper = new StudentMapper(connection);
+                for (const student of students) {
+                    student.createTime = new Date();
+                    student.updateTime = new Date();
+                    await studentMapper.updateByKeySelective(student);
+                }
+                await connection.commit();
+                return new Promise<void>((resolve, reject) => resolve());
+            } catch (e) {
+                await connection.rollback();
+                return new Promise<void>((resolve, reject) => reject(e));
+            }
+        } catch (e) {
+            return new Promise<void>((resolve, reject) => reject(e));
+        } finally {
+            if (connection) {
+                try {
+                    await connection.release();
+                } catch (releaseError) {
+                    // nothing to do.
+                    console.error(releaseError);
+                }
+            }
+        }
+    }
+
+    private async getStudents(): Promise<Student[]> {
+        let connection: IConnection;
+        try {
+            connection = await this.connectionFactory.getConnection();
+            const studentMapper = new StudentMapper(connection);
+            const query = DynamicQuery.createIntance<Student>();
+            const idDescSort = new SortDescriptor<Student>((s) => s.id, SortDirection.DESC);
+            query.addSorts(idDescSort);
+            const students = await studentMapper.selectByDynamicQuery(query);
+            return new Promise<Student[]>((resolve, reject) => resolve(students));
+        } catch (e) {
+            return new Promise<Student[]>((resolve, reject) => reject(e));
         } finally {
             if (connection) {
                 try {
